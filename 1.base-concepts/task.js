57a2f68ea24507e72ef10614e65ad3aeff543ac4
SaWeR <valera.sawer@yandex.ru>
2023-09-06 20:55:09 +0300

    First homework completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,36 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let d = (Math.pow(b, 2)-4*a*c);
+  let radical;
+  let sqrtFirst;
+  let sqrtSecond;
+
+  if (d > 0) {
+    sqrtFirst = (-b + Math.sqrt(d) )/(2*a);
+    sqrtSecond = (-b - Math.sqrt(d) )/(2*a);
+    arr.push(sqrtFirst)
+    arr.push(sqrtSecond)
+
+    return arr;
+  }
+  else if (d === 0) {
+    radical = (-b/(2*a));
+    arr.push(radical)
+    return arr;
+  }
+  else {
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let p = (percent / 100) / 12;
+  let s = amount - contribution;
+  let n = countMonths;
+  let monthlyPayment = s * (p + (p / (((1 + p) ** n) - 1)));
   
+  return (+(monthlyPayment * n).toFixed(2)) 
 }
+
+calculateTotalMortgage(10, 0, 50000, 12);
