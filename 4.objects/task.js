486b6ac26319e629fdaa7fdaad3b0bdb3d0a86db
SaWeR <valera.sawer@yandex.ru>
2023-09-25 22:02:07 +0300

    Task 'Objects' completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks === undefined) {
+        return 0;
+    }
+
+    else if (this.hasOwnProperty("marks")) {
+        this.marks.push(...marks)
+    }   
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks === undefined || this.marks === null || this.marks.length === 0) {
+        return 0;
+    }
+    return this.marks.reduce((acc, value) => acc + value, 0) / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
